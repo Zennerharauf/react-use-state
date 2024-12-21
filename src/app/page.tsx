efb9d21080764d21830171702d699222e092e3f6
Zennerharauf <163380432+Zennerharauf@users.noreply.github.com>
2024-12-21 21:51:24 +0500

    use state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,69 @@
-import Image from "next/image";
+"use client"
+import React, { useState, useEffect } from "react";
+// import { useClient } from "sanity";
+ 
+const app= () => {
+  const [count,setCount] = useState(0) //0 JO RKHA HUA WOHI VALUE AIGI       //count value ko hold krka rkhega jo bhe value aigi ya ha state variable setCount state updation function h y kam krega k jab + p click krega tw ya value chNGE KREGA
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+//first --- specifice function side effect ya action
+//second --- clean up
+//third --- list of dependencies
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+// useEffect(() => {    
+// first
+// return() => {
+//   second 
+// }
+// }, [third])
+
+//variant - 1
+// useEffect(() => {
+//   alert('i will run on each runder')    //jb bhe page lod hog y CHLEG ya aik specific kam hogya  like jb bhe counter ko plus kra page render huA tw isna phla ya msg dya 
+// }) but y KOI achi pracice h h first time p hojai bar bar na hooo
+
+//variant - 2
+// useEffect(() => {
+//   alert('i will run on each runder')     
+// }, [])      //,[] lgna sa ab ya bar bar nh aiga dependency lga di       //2 bar nh lna tw next config m jk react strict mode ko false krdega 
+
+//variant -- 3
+useEffect(() => {
+  alert('i will run on each runder')     
+}, [count]) 
+  
+return(
+  <div className="flex justify-center items-center h-screen flex-col">
+    <h1>counter</h1>
+
+    <div className="w-96 py-8 bg-slate-300 text-black flex justify-center items-center gap-6">
+
+    <button onClick={() => setCount(count + 1)}>+</button> 
+    {/* //button k andr on click kra iska andr   arrow function dLa aur setcount k function ko cLL KRA ab ya count ko + krega*/}
+    {count}
+
+    <button onClick={() => setCount(count - 1)}>-</button>
+  </div>
+  </div>
+)
 }
+
+export default app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
